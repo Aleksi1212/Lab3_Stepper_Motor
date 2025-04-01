bdbcb2100dc1aacbe8448382566b878cf0406ea3
Aleksi Noro <aleksinoro@gmail.com>
2025-04-01 12:33:45 +0300

    use read_line function

--- src/main.c
+++ src/main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "pico/stdlib.h"
 #include "hardware/pwm.h"
 
+#include "../include/stdio.h"
+
 int main(void) {
+    stdio_init_all();
+    
+    while (true) {
+        char *test = NULL;
+        int status = read_line(&test);
+        printf("%s\n", test);
+    }
     return 0;
 }
