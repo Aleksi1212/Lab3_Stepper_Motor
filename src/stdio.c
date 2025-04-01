732e3762c722baf0cf12670a4d982f516ee7fc38
Aleksi Noro <aleksinoro@gmail.com>
2025-04-01 12:33:54 +0300

    read_line function

---
+++ src/stdio.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../include/stdio.h"
+
+int read_line(char **dst)
+{
+    int initial_size = 64;
+    int pos = 0;
+    
+    *dst = (char *)malloc(initial_size * sizeof(char));
+    if (dst == NULL)
+        return 1;
+    
+    char chr;
+    while((chr = getchar()) != EOF && chr != '\n' && chr != '\r')
+    {
+        if (pos >= initial_size)
+        {
+            initial_size *= 2;
+            char *tmp = (char *)realloc(*dst, initial_size * sizeof(char));
+            if (tmp == NULL) {
+                free(dst);
+                return 1;
+            }
+
+            *dst = tmp;
+            free(tmp);
+        }
+
+        printf("%c", chr);
+        (*dst)[pos++] = chr;
+    }
+    (*dst)[pos] = '\0';
+
+    printf("\n");
+    return 0;
+}
