df52bfa9f9b53c0eda9ba2f5798e70bc66a96f14
Aleksi Noro <aleksinoro@gmail.com>
2025-04-05 22:08:06 +0300

    constants.h

---
+++ include/constants.h
@@ -0,0 +1,14 @@
+#ifndef CONSTANTS_h
+#define CONSTANTS_h
+
+#define OPTO_FORK 28
+
+#define IN_1 2
+#define IN_2 3
+#define IN_3 6
+#define IN_4 13
+
+#define STEP_COUNT 8
+#define STEPS_COUNT 4
+
+#endif
